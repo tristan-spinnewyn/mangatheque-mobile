2b6af7de618225603ff36c6b0818eef85bb23d2e
tristan <tristan.spinnewyn@outlook.fr>
2022-06-26 20:10:50 +0200

    add tome view

--- api/tomeApi.js
+++ api/tomeApi.js
@@ -1,8 +1,52 @@
-import {url_api} from "./config";
+import {getHeader, url_api} from "./config";
 import axios from "axios";
 
 export async function getLastTome(){
     const url = `${url_api}/tome/new`
+    const res = await axios.get(url)
+
+    return res.data
+}
+export async function addTomeInCollection(id){
+    const HEADER = await getHeader()
+    const url = `${url_api}/tome/${id}/add`
+    const res = await axios.post(url,null,HEADER)
+
+    return res.data
+}
+
+export async function delTomeInCollection(id){
+    const HEADER = await getHeader()
+    const url = `${url_api}/tome/${id}/delete`
+    const res = await axios.delete(url,HEADER)
+
+    return res.data
+}
+
+export async function getTomeInCollection(id){
+    const HEADER = await getHeader()
+    const url = `${url_api}/tome/${id}/collection`
+    const res = await axios.get(url,HEADER)
+
+    return res.data
+}
+
+export async function getTomeUserApi(){
+    const HEADER = await getHeader()
+    const url = `${url_api}/tome/collection`
+    const res = await axios.get(url,HEADER)
+
+    return res.data
+}
+
+export async function getNextTome(){
+    const url = `${url_api}/tome/next`
+    const res = await axios.get(url)
+
+    return res.data
+}
+export async function getTomeById(id){
+    const url = `${url_api}/tome/${id}`
     const res = await axios.get(url)
 
     return res.data
--- pages/Tome.js
+++ pages/Tome.js
@@ -1,11 +1,86 @@
-import {Text, View} from "react-native";
+import {Dimensions, Image, ScrollView, Text, TouchableOpacity, View} from "react-native";
 import * as React from 'react';
+import {useEffect, useState} from "react";
+import {addTomeInCollection, delTomeInCollection, getTomeById, getTomeInCollection} from "../api/tomeApi";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function Tome({route}){
+export default function Tome({route,navigation}){
     const { itemId, otherParam } = route.params;
+    const width = Dimensions.get('window').width
+    const [tome,setTome] = useState({id:'',numero:'',desc:'',nbpage:'',dateSortie:new Date(),imageCouverture:'',isbn:'',
+        edition:{id:'',nameEdition:'',statut:'',serie:{id:'',nameSeries:''},
+            avis:[{id:'',commantaire:'',signalee:'',user:{pseudonyme:''}}]}
+    })
+    const [collection,setCollection] = useState(false)
+    const getTome = async () => {
+        const data = await getTomeById(itemId)
+        await getTomeCollection()
+        setTome(data[0])
+
+    }
+    const getTomeCollection = async()=>{
+        const token = AsyncStorage.getItem('token')
+        if(token){
+            const data = await getTomeInCollection(itemId)
+            if(data[0]){
+                setCollection(true)
+            }
+        }
+    }
+    const addTome = async()=>{
+        const token = AsyncStorage.getItem('token')
+        if(token){
+            await addTomeInCollection(itemId)
+            setCollection(true)
+        }
+    }
+    const delTome = async()=>{
+        const token = AsyncStorage.getItem('token')
+        if(token){
+            await delTomeInCollection(itemId)
+            setCollection(false)
+        }
+    }
+    useEffect(()=>{
+        getTome()
+    },[itemId])
     return(
-        <View>
-            <Text>{itemId}</Text>
+        <View style={{flex:1}}>
+            <Text style={{ fontSize:25}}>{tome.edition.serie.nameSeries} - Tome n°{tome.numero}</Text>
+            {collection ? <TouchableOpacity style={{backgroundColor:'#db4a2b', height:40,width:300}} onPress={delTome}><Text>Supprimer le tome !</Text></TouchableOpacity> :
+                <TouchableOpacity onPress={addTome} style={{backgroundColor:'#db4a2b', height:40,width:300}}><Text>Ajouter le tome !</Text></TouchableOpacity>}
+            <View style={{ marginTop:15 }}>
+                <Text style={{fontSize:20}}>Série</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('Serie',{
+                    itemId:tome.edition.serie.id
+                })} style={{height:50, borderBottomWidth:1, borderBottomColor:'#CCCCCC'}}>
+                    <Text>{tome.edition.serie.nameSeries}</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{ marginTop:15 }}>
+                <Text style={{fontSize:20}}>Edition</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('Edition',{
+                    itemId:tome.edition.id
+                })} style={{height:50, borderBottomWidth:1, borderBottomColor:'#CCCCCC'}}>
+                    <Text>{tome.edition.nameEdition}</Text>
+                </TouchableOpacity>
+            </View>
+            <ScrollView style={{flex:1,width:width}}>
+                <View style={{justifyContent: 'center', alignItems: 'center' }}>
+                    <Image style={{
+                        width: width * 0.5,
+                        height:'auto',
+                        aspectRatio: 1, // <-- this
+                        resizeMode: 'contain', //optional
+                    }} source={{uri: tome.imageCouverture}}/>
+                </View>
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <Text>Synopsis: {tome.desc}</Text>
+                    <Text>ISBN: {tome.isbn}</Text>
+                    <Text>Date de sortie: {new Date(tome.dateSortie).toLocaleDateString()}</Text>
+                    <Text>Nombre de page: {tome.nbpage}</Text>
+                </View>
+            </ScrollView>
         </View>
     )
 }
